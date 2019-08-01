bb4ee0058c66e89d22cd49464dad2ef4dd621e58
tinnakorn941 <34223443+tinnakorn941@users.noreply.github.com>
2019-08-01 11:00:03 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const config = require('./config');
 const { Message, OpType, Location } = require('../curve-thrift/line_types');
 //let exec = require('child_process').exec;
 
-const myBot = ['ueacedbe88bf6e2c5cf6188b3a4a26e18','u5a8f1f79740e3dc876a13ab7f7381340'];
+const myBot = ['u63fb39f39813b9e88d3f20e05f642955','u5a8f1f79740e3dc876a13ab7f7381340'];
 const banList = [];//Banned list
 var groupList = new Array();//Group list
 var vx = {};var midnornama,pesane,kickhim;var waitMsg = "no";//DO NOT CHANGE THIS
